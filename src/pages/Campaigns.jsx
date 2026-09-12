40edd2f3211e12ba095b0c1f5a8f342f27d74866
Javeria <meetjaveriajavaid@gmail.com>
2026-09-12 11:44:12 +0500

    Fix JSX structure in Campaigns page

--- src/pages/Campaigns.jsx
+++ src/pages/Campaigns.jsx
@@ -301,6 +301,7 @@ export default function Campaigns() {
             </div>
             
 
+          </div>
           <div className="camp-hero__visual">
             <div className="camp-hero__photo-main">
               <img src="https://images.unsplash.com/photo-1488521787991-ed7bbaae773c?w=700&q=80" alt="Alkhidmat Campaign" />
